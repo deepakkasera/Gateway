6e12adad08eb406ea88acc09a8f0bead7d68f75c
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-02-25 14:29:54 +0530

    Gateway

---
+++ src/main/java/com/example/gatewayworkshop25feb/GatewayWorkshop25FebApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayworkshop25feb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayWorkshop25FebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayWorkshop25FebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gatewayworkshop25feb/GatewayWorkshop25FebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayworkshop25feb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayWorkshop25FebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
